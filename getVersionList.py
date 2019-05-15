7371fef84d71e203b9d2cdb794cf86c4b6980224
VitaminC <VItamineCi@163.com>
2019-05-15 08:41:32 +0800

    success getVersion

--- getVersionList.py
+++ getVersionList.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import re
+import json
+import os
 
 def getVersion(versionHtml):
     version = re.search(r'<h4 class="version-history__item__version-number">(.*?)</h4>',versionHtml,re.M|re.I).group(1)
     time = re.search(r'<time (.*)>(.*?)</time>',versionHtml,re.M|re.I).group(2)
     dict = {'version':version,'time':time}
     return dict
+
+# 将记录存储
+def saveData(appVersionList):
+    jsObj = json.dumps(appVersionList)
+    data = open('version.json','w')
+    data.write(jsObj)
+    data.close()
 
 
 # 无需吊起浏览器)
@@ -22,7 +31,7 @@ url = "https://itunes.apple.com/cn/app/%E5%AD%A6%E4%B9%A0%E5%BC%BA%E5%9B%BD/id14
 browser.get(url)
 
 # 根据id找到标签并模拟点击操作
-browser.find_element_by_id("modal-trigger-ember228").click()
+browser.find_element_by_id("modal-trigger-ember231").click()
 
 # appVersions = browser.find_element_by_class_name("version-history__item")
 # print('源码为：'+ appVersions.get_attribute('innerHTML'))
@@ -44,4 +53,18 @@ for appVersion in appVersionList:
     print("\nNo%d" %i)
     print("version: " + appVersion['version'] + "\ntime:" + appVersion['time'])
     i = i + 1
+
+if os.path.exists('version.json') == True:
+    dataFile = open('version.json','r')
+    listFile = json.load(dataFile)
+    if appVersionList[0] == listFile[0]:
+        print("App未更新")
+    else:
+        print("App已更新")
+        print("最新版本为：%s" %appVersionList[0]['version'])
+        print("更新日期为：%s" %appVersionList[0]['time'])
+        saveData(appVersionList)
+else:
+    saveData(appVersionList)
+
 
